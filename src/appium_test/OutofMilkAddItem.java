bf463416cd8db7a26092200e8f3349d3c59d156a
natam <natam@users.noreply.github.com>
2017-07-26 19:19:38 +0200

    upload project

---
+++ src/appium_test/OutofMilkAddItem.java
@@ -0,0 +1,87 @@
+package appium_test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class OutofMilkAddItem {
+
+ WebDriver driver;
+
+ @BeforeTest
+ public void setUp() throws MalformedURLException {
+  // Created object of DesiredCapabilities class.
+  DesiredCapabilities capabilities = new DesiredCapabilities();
+
+  // Set android deviceName desired capability. Set your device name.
+  capabilities.setCapability("deviceName", "ZX1B32FFXF");
+
+  // Set BROWSER_NAME desired capability. It's Android in our case here.
+  capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
+
+  // Set android VERSION desired capability. Set your mobile device's OS version.
+  capabilities.setCapability(CapabilityType.VERSION, "4.4.2");
+
+  // Set android platformName desired capability. It's Android in our case here.
+  capabilities.setCapability("platformName", "Android");
+
+  // Set android appPackage desired capability. It is
+  // com.android.calculator2 for calculator application.
+  // Set your application's appPackage if you are using any other app.
+  capabilities.setCapability("appPackage", "com.capigami.outofmilk");
+
+  // Set android appActivity desired capability. It is
+  // com.android.calculator2.Calculator for calculator application.
+  // Set your application's appPackage if you are using any other app.
+  capabilities.setCapability("appActivity", "com.capigami.outofmilk.MainActivity");
+
+  // Created object of RemoteWebDriver will all set capabilities.
+  // Set appium server address and port number in URL string.
+  // It will launch calculator app in android device.
+  driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+  driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+ }
+
+ @Test
+ public void Item_add() {
+  
+  driver.findElement(By.xpath("//android.widget.EditText[contains(@resource-id,'input_field')]")).sendKeys("test_item");
+  driver.findElement(By.xpath("//android.widget.ImageButton[contains(@resource-id,'submit')]")).click();
+  driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'action_sync')]")).click();
+
+  // Get result from result text box.
+  
+  boolean present=isElementPresent(By.xpath("//android.widget.TextView[contains(@resource-id,'description') and @text='Test_item']"));
+  System.out.println("New item was added : " + present);
+
+ } 
+
+ protected boolean isElementPresent(By by) {	    
+	    
+		try {
+
+			driver.findElement(by);
+
+			return true;
+
+		} catch (NoSuchElementException e) {
+
+			return false;
+		}
+	}
+ @AfterTest
+ public void End() {
+  driver.quit();
+ }
+}
